47c49d2b108c434cf50ef9051fba4b35b7f4125a
WangeciM <41282690+WangeciM@users.noreply.github.com>
2018-08-01 10:39:08 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,3 +4,8 @@ words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
 randomPhrase
+
+#reverse both the order of the 4 words and the order of the letters in each word
+
+reversePhrase = randomPhrase[::-1]
+print(reversePhrase)
